9cfcacfc5067184e2212d481e9d37871f912641c
s26856-pj <146528048+s26856-pj@users.noreply.github.com>
2023-12-26 13:15:37 +0100

    Add files via upload

---
+++ Commit/Commit/Commit.cpp
@@ -0,0 +1,18 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+
+void przypomnij_o_commit() {
+    MessageBox(NULL, _T("Klaudia Słońce, zrób commit!"), _T("Przypomnienie o commicie"), MB_ICONINFORMATION | MB_OK);
+}
+
+int main() {
+    
+    DWORD czas_przypomnienia = 20 * 60 * 1000;
+
+    while (true) {
+        Sleep(czas_przypomnienia);
+        przypomnij_o_commit();
+    }
+
+    return 0;
+}
